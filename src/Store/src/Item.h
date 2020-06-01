a95031abca93efed5892966d5dbc679444fe3806
R. van Elst <vanelst@dejongduke.nl>
2020-06-01 23:13:05 +0200

    Add more items

--- src/Store/src/Item.h
+++ src/Store/src/Item.h
@@ -49,6 +49,15 @@ private:
     Item Temple {"Temple", "e", "E", CookieNumber(150000), CookieNumber(1500), 55};
     Item WizardTower {"Wizard Tower", "w", "W", CookieNumber(300000), CookieNumber(2000), 60};
     Item Shipment {"Shipment", "o", "O", CookieNumber(4500000), CookieNumber(2500), 65};
+    Item Alchemylab {"Alchemy Lab", "a", "A", CookieNumber("900000000"), CookieNumber(3500), 90};
+    Item Portal {"Portal", "p", "P", CookieNumber("4500000000"), CookieNumber(5000), 130};
+//    Item Timemachine {"Time Machine", "i", "I", CookieNumber(), CookieNumber(), };
+//    Item Antimattercondenser {"Antimatter Condenser", "y", "Y", CookieNumber(), CookieNumber(), };
+//    Item Prism {"Prism", "Y", "Y", CookieNumber(), CookieNumber(), };
+//    Item Chancemaker {"Chance maker", "z", "Z", CookieNumber(), CookieNumber(), };
+//    Item FractalEngine {"Fractal Engine", "H", "H", CookieNumber(), CookieNumber(), };
+//    Item JavascriptConsole {"Javascript Console", "j", "J", CookieNumber(), CookieNumber(), };
+
     std::vector<Item> allItems {
         Key,
         Grandma,
@@ -59,6 +68,8 @@ private:
         Temple,
         WizardTower,
         Shipment,
+        Alchemylab,
+        Portal,
     };
 
 public:
