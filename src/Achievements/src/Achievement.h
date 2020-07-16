37135fd0e3c519f703491eab91bcfa76d00ceff1
R. van Elst <vanelst@dejongduke.nl>
2020-07-16 20:46:34 +0200

    fix folder for stylecheck

--- src/Achievements/src/Achievement.h
+++ src/Achievements/src/Achievement.h
@@ -4,9 +4,12 @@
 
 #ifndef C_OOKIECLIKER_ACHIEVEMENT_H
 #define C_OOKIECLIKER_ACHIEVEMENT_H
+#include <string>
 class Achievement {
 public:
 
+    virtual void apply() = 0;
+    virtual std::string description() = 0;
 
 };
 #endif //C_OOKIECLIKER_ACHIEVEMENT_H
