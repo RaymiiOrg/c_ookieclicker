2321172f278d89fc30f66836ed26b0d143065668
R. van Elst <vanelst@dejongduke.nl>
2021-02-08 13:37:13 +0100

    Add upgradeview to view tests

--- src/Screen/tst/MainView-test.cpp
+++ src/Screen/tst/MainView-test.cpp
@@ -10,8 +10,9 @@ struct ScreenTestSuite : public ::testing::Test
     ItemStore store;
     Inventory inventory;
     std::string InventoryInputKey = "2";
-    std::string AchievementInputKey = "3";
-    std::string OptionsInputKey = "4";
+    std::string UpgradeInputKey = "3";
+    std::string AchievementInputKey = "4";
+    std::string OptionsInputKey = "5";
 };
 
 TEST_F(ScreenTestSuite, defaultScreen)
@@ -29,14 +30,17 @@ TEST_F(ScreenTestSuite, switchScreen)
     MainView screen1(&msg, &wallet, &inventory, &store);
     MainView screen2(&msg, &wallet, &inventory, &store);
     MainView screen3(&msg, &wallet, &inventory, &store);
+    MainView screen4(&msg, &wallet, &inventory, &store);
     //act
     screen1.handleInput(InventoryInputKey);
-    screen2.handleInput(AchievementInputKey);
-    screen3.handleInput(OptionsInputKey);
+    screen2.handleInput(UpgradeInputKey);
+    screen3.handleInput(AchievementInputKey);
+    screen4.handleInput(OptionsInputKey);
     //assert
     ASSERT_EQ(screen1.activeView, dynamic_cast<View *>(&screen1.inventoryview));
-    ASSERT_EQ(screen2.activeView, dynamic_cast<View *>(&screen2.achievementlistview));
-    ASSERT_EQ(screen3.activeView, dynamic_cast<View *>(&screen3.optionsview));
+    ASSERT_EQ(screen2.activeView, dynamic_cast<View *>(&screen2.upgradeview));
+    ASSERT_EQ(screen3.activeView, dynamic_cast<View *>(&screen3.achievementlistview));
+    ASSERT_EQ(screen4.activeView, dynamic_cast<View *>(&screen4.optionsview));
 }
 
 
