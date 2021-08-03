d438cc50dca44ec4f29fcb15a18e354dc3f604db
R. van Elst <vanelst@dejongduke.nl>
2021-08-03 23:30:19 +0200

    Fix run_all_tests script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir -p cmake-build-debug
-cd cmake-build-debug
-
 if [[ $1 == "runcmake" ]]; then
+  mkdir -p cmake-build-debug-no-boost
+  pushd cmake-build-debug-no-boost
   cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -DBUILD_TESTS=Y
   make -j8 all
   if [[ $? != 0 ]]; then
@@ -14,6 +13,8 @@ if [[ $1 == "runcmake" ]]; then
 fi
 
 if [[ $1 == "runcmakeboost" ]]; then
+  mkdir -p cmake-build-debug-with-boost
+  pushd cmake-build-debug-with-boost
   cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -DBUILD_TESTS=Y -DUSEBOOST_MPP=Y
   make -j8 all
   if [[ $? != 0 ]]; then
